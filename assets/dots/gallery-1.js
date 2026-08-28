ad39728050f6e629ae522d8d95dd8cb2d8929283
gptjyp-ops <gpt.jyp@gmail.com>
2026-08-28 10:57:33 +0900

    DOTS 로고 전체 시안 이미지 1차 정리

---
+++ assets/dots/gallery-1.js
@@ -0,0 +1,9 @@
+window.DOTS_IMAGES=(window.DOTS_IMAGES||[]);window.DOTS_IMAGES.push(["data:image/jpeg;base64,base64: /tmp/dots-gallery/1.jpg: No such file or directory
+","data:image/jpeg;base64,base64: /tmp/dots-gallery/2.jpg: No such file or directory
+","data:image/jpeg;base64,base64: /tmp/dots-gallery/3.jpg: No such file or directory
+","data:image/jpeg;base64,base64: /tmp/dots-gallery/4.jpg: No such file or directory
+","data:image/jpeg;base64,base64: /tmp/dots-gallery/5.jpg: No such file or directory
+","data:image/jpeg;base64,base64: /tmp/dots-gallery/6.jpg: No such file or directory
+","data:image/jpeg;base64,base64: /tmp/dots-gallery/7.jpg: No such file or directory
+","data:image/jpeg;base64,base64: /tmp/dots-gallery/8.jpg: No such file or directory
+"]);
